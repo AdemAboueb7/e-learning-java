d5e64be1f815a7081d317537df465608ae06c687
Adem_Aboueb <adem.aboueb@esprit.tn>
2025-04-07 18:16:08 +0100

    Update IServices.java
    ajout : - throws SQLException en ligne 7,9,10
               - List<T> getAll() throws SQLException;
               -  import java.sql.SQLException;

--- src/main/java/tn/elearning/services/IServices.java
+++ src/main/java/tn/elearning/services/IServices.java
@@ -1,12 +1,17 @@
 package tn.elearning.services;
 
 import java.util.List;
+import java.sql.SQLException;
 
 public interface IServices<T> {
-    void ajouter(T t);
+    void ajouter(T t) throws SQLException ;
     void supprimerParId(int id);
-    void supprimer(T t);
-    void modifier(int id);
+    void supprimer(T t) throws SQLException ;
+    void modifier(int id) throws SQLException ;
     List<T> recuperer();
-    T recupererParId(int id);
+    T recupererParId(int id);  
+    List<T> getAll() throws SQLException;
+
 }
+
+
