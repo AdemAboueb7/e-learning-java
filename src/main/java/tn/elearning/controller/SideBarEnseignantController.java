74a77b0fb534371bbfa7f200a1542056d8604837
oumaimaghediri <oumaima.ghediri@esprit.tn>
2025-04-29 21:39:50 +0100

    changement cours v2

--- src/main/java/tn/elearning/controller/SideBarEnseignantController.java
+++ src/main/java/tn/elearning/controller/SideBarEnseignantController.java
@@ -2,12 +2,9 @@ package tn.elearning.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import tn.elearning.utils.NavigationUtil;
-import tn.elearning.utils.UserSession;
 
 import java.io.IOException;
 
@@ -16,6 +13,7 @@ public class SideBarEnseignantController {
     public Button coursBtn;
     public Button moduleBtn;
     public Button chapitreBtn;
+    public Button dashboardBtn;
     @FXML
     private Button articles;
 
@@ -33,30 +31,11 @@ public class SideBarEnseignantController {
 
     @FXML
     void handleArticlesAction(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/VoirArticlesTeacher.fxml"));
-            Parent root = loader.load();
-            articles.getScene().setRoot(root);
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
 
     }
 
     @FXML
     void handleDeconnexionAction(ActionEvent event) {
-        UserSession.getInstance().clear();  // Cette méthode efface l'utilisateur et l'ID de session
-
-        // Rediriger vers la page de connexion (Signin.fxml)
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Signin.fxml"));
-            Parent root = loader.load();
-            // Mettre à jour la scène actuelle pour afficher la page de connexion
-            sidebar.getScene().setRoot(root);  // "sidebar" est utilisé ici pour obtenir la scène actuelle
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -67,18 +46,13 @@ public class SideBarEnseignantController {
 
     @FXML
     void handleProfilAction(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/updateenseignant.fxml"));
-            Parent root = loader.load();
-            sidebar.getScene().setRoot(root);
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
 
     }
 
+    @FXML
+    void handleusersAction(ActionEvent event) {
 
+    }
     @FXML
     private void handleCoursAction(ActionEvent event) {
         try {
@@ -101,6 +75,14 @@ public class SideBarEnseignantController {
     private void handleChapitreAction(ActionEvent event) {
         try {
             NavigationUtil.navigateToChapitre();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleDashboardAction(ActionEvent event) {
+        try {
+            NavigationUtil.navigateToDashboard();
         } catch (IOException e) {
             e.printStackTrace();
         }
